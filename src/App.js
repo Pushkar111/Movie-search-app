fd05bde413f5ea921eaa9de4901e2d654e23a530
Pushkar Modi <116958355+Pushkar111@users.noreply.github.com>
2025-04-06 18:59:19 +0530

    Refactor App component to include FavoritesProvider and MoviesFavorites route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import { MoviesNavbar } from "./components/MoviesNavbar";
 import { MovieDetail } from "./components/MovieDetail";
 import { MoviesHome } from "./components/MoviesHome";
+import { MoviesFavorites } from "./components/MoviesFavorites";
+import { FavoritesProvider } from "./context/FavoritesContext";
 
 function App() {
     return (
-        <div className="App">
-            <MoviesNavbar/>
-            <Routes>
+        <FavoritesProvider>
+            <div className="App">
+                <MoviesNavbar/>
+                <Routes>
                 <Route path="/" element={<MoviesHome/>}></Route>
-                <Route path="/moviedetail/:imdbID" element={<MovieDetail/>}></Route>
-            </Routes>
-        </div>
+                    <Route path="/moviedetail/:imdbID" element={<MovieDetail/>}></Route>
+                    <Route path="/favorites" element={<MoviesFavorites/>}></Route>
+                </Routes>
+            </div>
+        </FavoritesProvider>
     );
 }
 
